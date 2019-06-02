6e6665e7e5732938a3bdd1a7da7fa2a8ee338a7a
Justin Ryder <2396658+justinryder@users.noreply.github.com>
2019-06-02 16:46:16 -0500

    Auto-commit

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const path = require('path');
 const shell = require('shelljs');
 const watch = require('watch');
 
-const execPromise = (...args) =>
-  new Promise((resolve, reject) =>
-    shell.exec(...args, (code, stdout, stderr) =>
-      code ?
-        reject(stderr) :
-        resolve(stdout)
-    ));
+const resolvedPromise = value => new Promise(resolve => resolve(value));
+
+const execPromise = (command) =>
+  new Promise(resolve => shell.exec(command, resolve));
 
 const pipe = (...fns) =>
   (arg) =>
@@ -36,25 +33,67 @@ const options = args.parse(process.argv);
 const gitAdd = () => execPromise('git add .');
 const gitCommit = () => execPromise('git commit -a -m "Auto-commit"');
 const gitPush = () => execPromise('git push');
+const gitHasChanges = () =>
+  execPromise('git diff --exit-code --quiet')
+    .then(code => Boolean(code));
 
-const shipIt = pipePromise(
-  gitAdd,
-  gitCommit,
-  gitPush,
+const log = message =>
+  value => console.log(message) || resolvedPromise(value);
+
+const rejectIfFalse = value =>
+  new Promise((resolve, reject) => value ? resolve(value) : reject(value));
+
+const rejectWithMessageIfFalse = message =>
+  value =>
+    new Promise((resolve, reject) => {
+      console.log('rejectWithMessageIfFalse', message, value);
+      return value ? resolve(value) : reject(message);
+    });
+
+const shipIt = arg =>
+  pipePromise(
+    log('Attempting to "ship it."'),
+    gitHasChanges,
+    rejectWithMessageIfFalse('git diff shows no changes'),
+    gitAdd,
+    gitCommit,
+    gitPush,
+  )(arg).then(null, message => console.log('Error "shipping it":', message));
+
+// shipIt();
+
+const matches = pattern =>
+  value =>
+    Boolean(value.match(pattern));
+
+const doesNotMatch = pattern =>
+  value =>
+    !matches(pattern)(value);
+
+const doesNotMatchAny = (...patterns) =>
+  value =>
+    patterns.map(pattern => doesNotMatch(pattern))
+      .every(noMatch => noMatch(value));
+
+watch.createMonitor(
+  path.join(__dirname, options.path),
+  {
+    filter: doesNotMatchAny(
+      /\.git/i,
+      /node_modules/i,
+    ),
+  },
+  monitor => {
+    monitor.on('created', (f, stat) => {
+      shipIt();
+    });
+
+    monitor.on('changed', (f, curr, prev) => {
+      shipIt();
+    });
+
+    monitor.on('removed', (f, stat) => {
+      shipIt();
+    });
+  }
 );
-
-shipIt();
-
-watch.createMonitor(path.join(__dirname, options.path), monitor => {
-  monitor.on('created', (f, stat) => {
-    shipIt();
-  });
-
-  monitor.on('changed', (f, curr, prev) => {
-    shipIt();
-  });
-
-  monitor.on('removed', (f, stat) => {
-    shipIt();
-  });
-});
